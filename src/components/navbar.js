a5aa3ad8b13c86a7406f94e37ab2a5c3e3c3198c
robinbenitezmora <robinbenitez1990@gmail.com>
2022-10-07 10:19:00 -0500

    Modify code lines in 'navbar.js'

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,11 @@
+import 'App.css';
+
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <h1 className="navbar__title">Weather Info</h1>
-    </nav>
-  );
+  <nav className="navbar">
+    <div className="container">
+      <h2 className="navbar-brand">Weather Info</h2>
+    </div>
+  </nav>
 }
 
 export default Navbar;
