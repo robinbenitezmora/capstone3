6e343385fecaec8a237185a7cdd44c563063144d
robinbenitezmora <robinbenitez1990@gmail.com>
2022-10-07 11:22:07 -0500

    Modify code lines in 'configureStore'

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import forecastReducer from './redux/forecast';
 import climateReducer from './redux/climate';
+import logger from 'redux-logger';
 
-const store = configureStore({
+const Store = configureStore({
   reducer: {
     forecast: forecastReducer,
     climate: climateReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
-export default store;
+export default Store;
